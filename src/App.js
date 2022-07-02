90050be257d46cc325177bdd8d0ff039ea04b60c
UDAYVEERUS <udayveerus348566@gmail.com>
2022-07-02 12:29:02 +0530

    unit evaluation

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Navbar from './components/navbar'
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/home"
+import Booking from "./Pages/booking.jsx";
+import Login from "./Pages/login.jsx"
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="Booking" element={<Booking />}></Route>
+        <Route path="Login" element={<Login />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Heading = styled.h2`
+  color: white;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: red;
+  padding: 10px 30px 10px 60px;
+  
+`;
+
+const Navbarwrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  gap: 30px;
+  color: red;
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  font-size: 30px;
+//   text-decoration: none;
+
+`;
+
+const Navbar = () => {
+  return (
+    <>
+      <Container>
+        <Heading>Movie App</Heading>
+        <Navbarwrapper>
+          <StyledLink to="/">HOME</StyledLink>
+          <StyledLink to="/Booking">BOOKING</StyledLink>
+          <StyledLink to="/Login">LOGIN</StyledLink>
+        </Navbarwrapper>
+      </Container>
+    </>
+  );
+};
+
+export default Navbar;
